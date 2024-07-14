4de3b27e4dd569877c35f6b22c4094581f3ba712
Salman Pro <skdevelopers@live.com>
2024-07-14 08:26:05 +0500

    Rex ERP Product corrected by Salman@thinkdelivers.com

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.vertical', ['title' => 'Edit Product'])
+@extends('layouts.vertical', ['title' => 'Edit Product', 'sub_title' => 'Products'])
 
 @section('content')
     <div class="container">
