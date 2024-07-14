ec53127559fab876510b9b23b8941ff4e759c2bf
Salman Pro <skdevelopers@live.com>
2024-07-14 08:32:42 +0500

    Rex ERP Category by Salman@thinkdelivers.com

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -3,11 +3,18 @@
 
 @section('content')
     <div class="container">
-        <h1>Create Category</h1>
-        <form action="{{ route('categories.store') }}" method="POST">
-            @csrf
-            @include('categories._form')
-            <button type="submit" class="btn btn-primary">Create</button>
-        </form>
+        <div class="grid lg:grid-cols-4 gap-6">
+            <div class="col-span-1 flex flex-col gap-6">
+                <div class="card p-6">
+                    <h1>Create Category</h1>
+
+                    <form action="{{ route('categories.store') }}" method="POST">
+                        @csrf
+                        @include('categories._form')
+                        <button type="submit" class="btn btn-primary">Create</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -3,12 +3,18 @@
 
 @section('content')
     <div class="container">
-        <h1>Edit Category</h1>
-        <form action="{{ route('categories.update', $category->id) }}" method="POST">
-            @csrf
-            @method('PUT')
-            @include('categories._form', ['category' => $category])
-            <button type="submit" class="btn btn-primary">Update</button>
-        </form>
+        <div class="grid lg:grid-cols-4 gap-6">
+            <div class="col-span-1 flex flex-col gap-6">
+                <div class="card p-6">
+                    <h1>Edit Category</h1>
+                    <form action="{{ route('categories.update', $category->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        @include('categories._form', ['category' => $category])
+                        <button type="submit" class="btn btn-primary">Update</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
